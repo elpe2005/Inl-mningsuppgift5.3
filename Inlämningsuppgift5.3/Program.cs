f9948f0136a0d8a8ccf6ed93aa3a08da6b5079a9
Bob156590 <gujo04002@utb.vaxjo.se>
2023-01-20 10:49:33 +0100

    Add project files.

---
+++ Inlämningsuppgift5.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uppgift51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frågor = { "Vad heter Sveriges huvudstad?", "vilket spel är bättre Rust eller Ark?", "Vad är bästa vapnet i CS?", "Vad är Tysklands huvudstad?" };
+            string[] svar = { "Stockholm", "Rust", "ak", "Berlin" };
+
+        }
+    }
+}
